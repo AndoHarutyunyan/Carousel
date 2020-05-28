909aa3e6779888a8d1c93eda29da3578d3a2088d
Ando Harutyunyan <60383016+AndoHarutyunyan@users.noreply.github.com>
2020-05-28 21:13:29 +0400

    Add files via upload

---
+++ carousel-JS.js
@@ -0,0 +1,46 @@
+const left=document.getElementById('left')
+const right=document.getElementById('right')
+const play=document.getElementById('play')
+const imgClass=document.getElementsByClassName('imgClass')
+const slayd=document.getElementsByClassName('slayder')
+let coll
+
+right.addEventListener('click',nextRight)
+function nextRight(){
+    for(let i=0; i<imgClass.length-1; i++){
+        if(imgClass[i].className==="imgClass orderName"){
+            imgClass[i].classList.remove('orderName')
+            return imgClass[i+1].classList.toggle('orderName')
+        }
+    }
+    if(imgClass[5].className==="imgClass orderName"){
+        imgClass[5].classList.toggle('orderName')
+    }else{
+        imgClass[1].classList.toggle('orderName')
+    }
+
+}
+left.addEventListener('click',NextLeft)
+function NextLeft() {
+    for(let i=1;i<imgClass.length;i++){
+        if(imgClass[i].className==="imgClass orderName"){
+            imgClass[i].classList.remove('orderName')
+            return imgClass[i-1].classList.toggle('orderName')
+        }
+    }
+    if(imgClass[0].className!=='imgClass orderName' ||imgClass[0].className==='imgClass orderName'){
+        imgClass[0].classList.remove('orderName')
+        imgClass[5].classList.toggle('orderName')
+    }
+
+}
+play.addEventListener('click',playPause)
+function playPause() {
+    if(slayd[0].className==='slayder 1'){
+        slayd[0].classList.remove('1')
+        clearInterval(coll)
+    }else{
+        slayd[0].classList.toggle('1')
+        coll=setInterval(nextRight,1000)
+    }
+}
